55e4e1ad85f539053d7810f8e1ca201b8e9d70ed
Jonathan Netley <jpbnetley@gmail.com>
2024-09-13 18:19:28 +0200

    check types and bundle concurrently

--- packages/web/esbuild.watch.config.mjs
+++ packages/web/esbuild.watch.config.mjs
@@ -1,10 +1,25 @@
 import { context } from 'esbuild'
 import { config } from './esbuild.shared.config.mjs'
 
+import { spawn } from'node:child_process';
+import readline from 'node:readline';
+
 async function runWatch() {
  const ctx = await context(config)
  await ctx.watch()
  console.log('Watching...');
 }
 
+function runTypeCheck() {
+  const child = spawn('npm', ['run', 'watch:check:types'])
+    readline.createInterface({
+        input: child.stdout
+    }).on('line', console.log);
+
+    readline.createInterface({
+        input: child.stderr,
+    }).on('line', console.log);
+}
+
+runTypeCheck()
 runWatch()
